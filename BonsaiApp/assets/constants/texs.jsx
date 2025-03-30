5125bafecdfdb8d614f9f11cb2aab3e3b2e6db83
Hoàng Công Nhựt Việt <156928605+NhutViet@users.noreply.github.com>
2025-03-30 19:48:03 +0700

    App

---
+++ BonsaiApp/assets/constants/texs.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const texs = () => {
+  return (
+    <View>
+      <Text>texs</Text>
+    </View>
+  );
+};
+
+export default texs;
+
+const styles = StyleSheet.create({});
